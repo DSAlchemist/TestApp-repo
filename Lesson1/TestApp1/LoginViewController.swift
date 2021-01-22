b8a5a76a4d8ab124c5619e6e3d94d3543877a6fb
DSAlchemist <ccdefs@gmail.com>
2021-01-22 16:29:05 +0300

    Lesson1

---
+++ Lesson1/TestApp1/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  TestApp1
+//
+//  Created by Danil G. on 23.12.2020.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var paswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // Do any additional setup after loading the view.
+    }
+    
+      
+    @objc func keyboardWillShow(notification: Notification) {
+        guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
+        scrollView.contentInset = insets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        let insets = UIEdgeInsets.zero
+        scrollView.contentInset = insets
+    }
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+    
+    
+    }
+    
+  
+
+}
